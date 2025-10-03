4f584b126b8e097167f49656eee2b0792142d186
Sergey Ryzhakov <novoposarantu1@gmail.com>
2025-10-03 16:13:27 +0300

    init

---
+++ App.Tests/T1_CustomDictionary/DualKeyDictionaryTests.cs
@@ -0,0 +1,60 @@
+namespace App.Tests.T1_CustomDictionary;
+
+[TestFixture]
+public class DualKeyDictionaryTests
+{
+    [Test]
+    public void SetAndGet_ByIntKey_Works_For_ReferenceType()
+    {
+        var dict = new DualKeyDictionary<string>();
+        dict[42] = "answer";
+        Assert.That(dict[42], Is.EqualTo("answer"));
+
+        dict[42] = "updated";
+        Assert.That(dict[42], Is.EqualTo("updated"));
+    }
+
+    [Test]
+    public void SetAndGet_ByStringKey_Works_For_ValueType()
+    {
+        var dict = new DualKeyDictionary<int>();
+        dict["count"] = 10;
+        Assert.That(dict["count"], Is.EqualTo(10));
+
+        dict["count"] = -5;
+        Assert.That(dict["count"], Is.EqualTo(-5));
+    }
+
+    [Test]
+    public void Get_Unknown_Int_Key_Throws()
+    {
+        var dict = new DualKeyDictionary<string>();
+        Assert.Throws<KeyNotFoundException>(() => { var _ = dict[777]; });
+    }
+
+    [Test]
+    public void Get_Unknown_String_Key_Throws()
+    {
+        var dict = new DualKeyDictionary<int>();
+        Assert.Throws<KeyNotFoundException>(() => { var _ = dict["nope"]; });
+    }
+
+    [Test]
+    public void String_Key_Null_Throws_On_Get_And_Set()
+    {
+        var dict = new DualKeyDictionary<string>();
+        Assert.Throws<ArgumentNullException>(() => { var _ = dict[null!]; });
+        Assert.Throws<ArgumentNullException>(() => dict[null!] = "x");
+    }
+
+    [Test]
+    public void Int_And_String_KeySpaces_Are_Independent()
+    {
+        var dict = new DualKeyDictionary<string>();
+        dict[1] = "one";
+        dict["1"] = "string-one";
+
+        Assert.That(dict[1], Is.EqualTo("one"));
+        Assert.That(dict["1"], Is.EqualTo("string-one"));
+    }
+}
---
+++ App.Tests/T2_WarehouseManager/WarehouseManagerTests.cs
@@ -0,0 +1,64 @@
+namespace App.Tests.T2_WarehouseManager;
+
+[TestFixture]
+public class WarehouseManagerTests
+{
+    [Test]
+    public void PrintBasicInfo_Prints_For_Electronics()
+    {
+        var items = new List<Electronics>
+        {
+            new Electronics(1, "Phone", 24),
+            new Electronics(2, "Laptop", 12)
+        };
+
+        var manager = new WarehouseManager<Electronics>(items);
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
+
+        manager.PrintBasicInfo();
+
+        var output = sw.ToString().Replace("\r\n", "\n");
+        Assert.That(output, Is.EqualTo(
+            "ID: 1, Name: Phone\n" +
+            "ID: 2, Name: Laptop\n"));
+    }
+
+    [Test]
+    public void PrintBasicInfo_Prints_For_Food()
+    {
+        var items = new List<Food>
+        {
+            new Food(10, "Milk", new DateTime(2030, 1, 1)),
+            new Food(20, "Bread", new DateTime(2026, 12, 31))
+        };
+
+        var manager = new WarehouseManager<Food>(items);
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
+
+        manager.PrintBasicInfo();
+
+        var output = sw.ToString().Replace("\r\n", "\n");
+        Assert.That(output, Is.EqualTo(
+            "ID: 10, Name: Milk\n" +
+            "ID: 20, Name: Bread\n"));
+    }
+
+    [Test]
+    public void Empty_List_Prints_Nothing()
+    {
+        var manager = new WarehouseManager<Electronics>(new List<Electronics>());
+        using var sw = new StringWriter();
+        Console.SetOut(sw);
+
+        manager.PrintBasicInfo();
+        Assert.That(sw.ToString(), Is.EqualTo(string.Empty));
+    }
+
+    [Test]
+    public void Ctor_Null_Items_Throws()
+    {
+        Assert.Throws<ArgumentNullException>(() => new WarehouseManager<Food>(null!));
+    }
+}
